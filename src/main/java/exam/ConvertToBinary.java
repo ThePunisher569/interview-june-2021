67b88394d3c46e3f7d23263368f92750c342b53a
ThePunisher569 <aatifdom10@gmail.com>
2021-06-22 12:51:00 +0530

    solved

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,32 @@
 package exam;
 
+import org.apache.commons.codec.binary.StringUtils;
+import org.apache.maven.surefire.shared.lang3.ArrayUtils;
+
 public class ConvertToBinary {
 
-  public String toBinary(final int n) {
+  public String toBinary(int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    String res = "";
+    try {
+      if(n<0){
+        throw new Exception("Number is negative");
+      }
+
+      int[] bin = new int[50];
+      int i = 0;
+      while(n>0){
+        bin[i++] = n%2;
+        n = n/2; 
+      }
+
+      for(int j = i-1;j >= 0;j--){    
+        res += String.valueOf(bin[j]);    
+      }  
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return res;
   }
 }
--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,10 +1,19 @@
 package exam;
 
+import java.util.Arrays;
+
 public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
 
+    int last = arr[arr.length-1];
+
+    for(int i=arr.length-1;i>0;i--){
+      arr[i] = arr[i-1];
+    }
+    arr[0] = last;
+    System.out.println(Arrays.toString(arr));
   }
 
 }
